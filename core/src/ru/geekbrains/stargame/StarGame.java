702c37ca540b5339c4092fbaf76a97eea7640351
Alex <xzolotarev@yandex.ru>
2018-09-14 20:13:15 +0300

    Changed the screen resolution, added game background

--- core/src/ru/geekbrains/stargame/StarGame.java
+++ core/src/ru/geekbrains/stargame/StarGame.java
@@ -9,25 +9,26 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class StarGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	Texture backgroud;
+	public static final int WIDTH = 800;
+	public static final int HEIGHT = 800;
+	public static final String TITLE = "Космическая Одиссея";
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		backgroud = new Texture("backgroundGame.jpg");
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0.4f, 1, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(backgroud,0,0,WIDTH, HEIGHT);
 		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
 	}
 }
