552b02912b5783212cde7b9b617b7f6b71ee75c2
Alex <xzolotarev@yandex.ru>
2018-09-17 01:59:57 +0300

    Height, width and title options are now located in the MenuScreen

--- desktop/src/ru/geekbrains/stargame/desktop/DesktopLauncher.java
+++ desktop/src/ru/geekbrains/stargame/desktop/DesktopLauncher.java
@@ -3,13 +3,14 @@ package ru.geekbrains.stargame.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import ru.geekbrains.stargame.StarGame;
+import ru.geekbrains.stargame.base.screen.MenuScreen;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = StarGame.WIDTH;
-		config.height = StarGame.HEIGHT;
-		config.title = StarGame.TITLE;
+		config.width = MenuScreen.WIDTH;
+		config.height = MenuScreen.HEIGHT;
+		config.title = MenuScreen.TITLE;
 		new LwjglApplication(new StarGame(), config);
 	}
 }
