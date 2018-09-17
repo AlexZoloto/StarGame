c2524146d8d974281d5c12b0ce10629bf5d06b2f
Alex <xzolotarev@yandex.ru>
2018-09-18 00:49:32 +0300

    Added 2 methods of movement using the arrows and wasd

--- core/src/ru/geekbrains/stargame/base/screen/MenuScreen.java
+++ core/src/ru/geekbrains/stargame/base/screen/MenuScreen.java
@@ -2,6 +2,7 @@ package ru.geekbrains.stargame.base.screen;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -16,8 +17,11 @@ public class MenuScreen extends Base2DScreen {
     public static final int WIDTH = 800;
     public static final int HEIGHT = 600;
     public static final String TITLE = "Космическая Одиссея";
-    Vector2 position;
-    Vector2 speed;
+    private Vector2 startPosition;
+    private Vector2 speed;
+    private float positionX;
+    private float positionY;
+
 
     public MenuScreen(Game game) {
         super(game);
@@ -29,8 +33,10 @@ public class MenuScreen extends Base2DScreen {
         batch = new SpriteBatch();
         backgroud = new Texture("backgroundGame.jpg");
         spaceship = new Texture("spaceship.png");
-        position = new Vector2(0f,0f);
-        speed = new Vector2(2f, 1f);
+        positionX = 0;
+        positionY = 0;
+        startPosition = new Vector2(0,0);
+        speed = new Vector2(2f, 4f);
     }
 
     @Override
@@ -40,9 +46,10 @@ public class MenuScreen extends Base2DScreen {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
         batch.draw(backgroud,0,0,WIDTH, HEIGHT);
-        batch.draw(spaceship, 0, 0);
+        batch.draw(spaceship, startPosition.x, startPosition.y);
         batch.end();
-        position.add(speed);
+        changePosition(startPosition, positionX, positionY, speed);
+        movesWhenKeyPressed();
     }
 
     @Override
@@ -54,7 +61,31 @@ public class MenuScreen extends Base2DScreen {
 
     @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        System.out.println("touchDown screenX = " + screenX + " screenY = " + (Gdx.graphics.getHeight() - screenY));
+        positionX = screenX;
+        positionY = (Gdx.graphics.getHeight() - screenY);
         return super.touchDown(screenX, screenY, pointer, button);
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        if (keycode == Input.Keys.LEFT) positionX -= speed.x;
+        if (keycode == Input.Keys.RIGHT) positionX += speed.x;
+        if (keycode == 19) positionY += speed.y;
+        if (keycode == 20) positionY -= speed.y;
+        return super.keyDown(keycode);
+    }
+    private void movesWhenKeyPressed(){
+        if (Gdx.input.isKeyPressed(Input.Keys.A)){
+            positionX -= speed.x;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.D)){
+            positionX += speed.x;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.S)){
+            positionY -= speed.y;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.W)){
+            positionY += speed.y;
+        }
     }
 }
