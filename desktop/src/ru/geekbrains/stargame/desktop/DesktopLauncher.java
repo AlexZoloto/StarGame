1e5b696bdb390815fc180d05555f5103b6cc8bc3
Alex <xzolotarev@yandex.ru>
2018-09-25 03:08:58 +0300

    Change size window game

--- desktop/src/ru/geekbrains/stargame/desktop/DesktopLauncher.java
+++ desktop/src/ru/geekbrains/stargame/desktop/DesktopLauncher.java
@@ -2,15 +2,15 @@ package ru.geekbrains.stargame.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
 import ru.geekbrains.stargame.StarGame;
-import ru.geekbrains.stargame.base.screen.MenuScreen;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = MenuScreen.WIDTH;
-		config.height = MenuScreen.HEIGHT;
-		config.title = MenuScreen.TITLE;
+		config.resizable = false;
+		config.width = 600;
+		config.height = 800;
 		new LwjglApplication(new StarGame(), config);
 	}
 }
